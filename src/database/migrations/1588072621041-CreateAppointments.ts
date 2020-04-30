9fb8175d0910911f486f0f8d5a5ca73d99d1d249
Agmar Torres <agmartorres@gmail.com>
2020-04-30 09:10:31 -0300

    Additing data of Appointments

--- src/database/migrations/1588072621041-CreateAppointments.ts
+++ src/database/migrations/1588072621041-CreateAppointments.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export default class CreateAppointments1588072621041
@@ -12,9 +13,10 @@ export default class CreateAppointments1588072621041
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
-            name: 'privider',
+            name: 'provider',
             type: 'varchar',
             isNullable: false,
           },
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,7 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+// eslint-disable-next-line no-unused-vars
 import Appointment from '../models/Appointments';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -14,27 +17,23 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  private appointmentsRepository: AppointmentsRepository;
-
-  constructor(appointmentsRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({ date, provider }: Request): Appointment {
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
       appointmentDate,
     );
 
     if (findAppointmentInSameDate) {
       throw Error('This appointment is already booked');
     }
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
 
+    await appointmentsRepository.save(appointment);
     return appointment;
   }
 }
