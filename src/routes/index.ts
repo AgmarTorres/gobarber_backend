e560be94319b4dc03c9c8d640af8207f10c78c0f
Agmar Torres <agmartorres@gmail.com>
2020-05-02 15:28:46 -0300

    adicionando token jwt

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import userRouter from './user.routes';
+import sessionRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', userRouter);
-
+routes.use('/sessions', sessionRouter);
 export default routes;
