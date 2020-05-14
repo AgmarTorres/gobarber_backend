77c8ed4ce0c98183f19ae86ca1c045bd5b3dddcb
Agmar Torres <agmartorres@gmail.com>
2020-05-14 12:25:03 -0300

    additing cors into application

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express, { Request, Response, NextFunction, Error } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -8,8 +9,9 @@ import AppError from './errors/AppError';
 
 const app = express();
 
-app.use('files', express.static(uploadConfig.directory));
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
