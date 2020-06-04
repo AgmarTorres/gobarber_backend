c75b166f3679d5b5fc902f5ca3e26aff7090eeef
Agmar Torres <agmartorres@gmail.com>
2020-06-03 22:18:06 -0300

    Adicionado o primeiro teste fake do Appointment

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentRepository.ts
@@ -0,0 +1,27 @@
+import { uuid } from 'uuidv4'
+import Appointment from '../../infra/typeorm/entities/Appointments';
+import  IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+import ICreateAppointmentsDTO from '@modules/appointments/dtos/ICreateAppointments'
+
+class AppointmentsRepository  implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointmentByDate = this.appointments.find( appointment => appointment.date == date)
+    return findAppointmentByDate
+  }
+
+  public async create({provider_id, date}: ICreateAppointmentsDTO): Promise<Appointment>{
+    const appointment = new Appointment();
+
+    Object.assign( appointment, { id: uuid(), date, provider_id});
+
+    appointment.id = uuid()
+    appointment.date = date;
+    appointment.provider_id = provider_id;
+    this.appointments.push( appointment)
+
+    return appointment
+  }
+}
+
+export default AppointmentsRepository;
--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,19 @@
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository'
+import CreateAppointmentService from './CreateAppointmentService';
 
 describe("CreateAppointment", () =>{
 
-  it('should to be able to create a new appoinment', () =>{
-    expect(1+ 2).toBe(3)
+  it('should to be able to create a new appoinment', async () =>{
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentRepository)
+
+    const appointment = await createAppointment.execute({
+        date: new Date,
+        provider_id: '1231231'
+    })
+
+    expect( appointment ).toHaveProperty('id')
+    expect(appointment.provider_id).toBe('1231231')
   })
 
   it('should no  to be able to create two appoinemtens in the same time', () =>{
